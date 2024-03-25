f4a08c13939cbfdadae078503bbe2a313557244f
wanjiku5 <awanjikunjeri@gmail.com>
2024-03-25 12:05:23 +0300

    first commit

---
+++ assignment.py
@@ -0,0 +1,24 @@
+
+
+
+
+from typing import Self
+
+
+class person:
+    def __init__(gender,name,age):
+        Self.name=name
+        Self.age=age
+        Self.gender=gender
+
+        name=input("enter  your name")
+        age=int(input("enter your age"))
+        gender=input("enter your gender")
+
+        name=input("enter your name")
+        age=input("age")
+        gender=input("enter your gender")
+
+        print("my name is", name,"i'm", age, "enter your genger", gender)
+
+
